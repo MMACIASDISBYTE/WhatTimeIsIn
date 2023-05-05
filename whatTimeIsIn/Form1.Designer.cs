a47ae652c54af1526df75420387bd2ab58fe3cff
mmaciasdisbyte <mmacias@disbyte.com>
2023-05-05 16:47:11 -0300

    se agrego estilo y fuente

--- whatTimeIsIn/Form1.Designer.cs
+++ whatTimeIsIn/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(90, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Your local Time";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("DS-Digital", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Menu;
+            label2.Location = new Point(34, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(268, 79);
+            label2.TabIndex = 1;
+            label2.Text = "16:12:23";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(940, 519);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "What time is in...";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
     }
 }
